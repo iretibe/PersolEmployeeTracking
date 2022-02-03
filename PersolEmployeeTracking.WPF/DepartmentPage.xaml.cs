d431399cb3794606b6ab986542b6e70fe1a5431c
Somad Yessoufou <yessouf2009@yahoo.fr>
2022-02-03 22:55:58 +0000

    Latest updates

--- PersolEmployeeTracking.WPF/DepartmentPage.xaml.cs
+++ PersolEmployeeTracking.WPF/DepartmentPage.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PersolEmployeeTracking.WPF.Databases;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace PersolEmployeeTracking.WPF
 {
@@ -22,6 +11,51 @@ namespace PersolEmployeeTracking.WPF
         public DepartmentPage()
         {
             InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        public Department department;
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDepartmentName.Text.Trim() == "")
+                MessageBox.Show("Please fill the Department Name area");
+            else
+            {
+                using (PersolEmployeeTrackingContext db = new PersolEmployeeTrackingContext())
+                {
+                    if (department != null && department.Id != 0)
+                    {
+                        Department update = new Department();
+                        update.DepartmentName = txtDepartmentName.Text;
+                        update.Id = department.Id;
+                        db.Departments.Update(update);
+                        db.SaveChanges();
+                        MessageBox.Show("Update was succesful");
+                    }
+                    else
+                    {
+                        Department dpt = new Department();
+                        dpt.DepartmentName = txtDepartmentName.Text;
+                        db.Departments.Add(dpt);
+                        db.SaveChanges();
+                        txtDepartmentName.Clear();
+                        MessageBox.Show("Department was Added");
+                    }
+                }
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (department != null && department.Id != 0)
+            {
+                txtDepartmentName.Text = department.DepartmentName;
+            }
         }
     }
 }
--- PersolEmployeeTracking.WPF/Views/DepartmentList.xaml.cs
+++ PersolEmployeeTracking.WPF/Views/DepartmentList.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
+﻿using PersolEmployeeTracking.WPF.Databases;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PersolEmployeeTracking.WPF.Views
 {
@@ -23,6 +14,60 @@ namespace PersolEmployeeTracking.WPF.Views
         public DepartmentList()
         {
             InitializeComponent();
+            using (PersolEmployeeTrackingContext db = new PersolEmployeeTrackingContext())
+            {
+                List<Department> list = db.Departments.OrderBy(x => x.DepartmentName).ToList();
+                gridDepartment.ItemsSource = list;
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            DepartmentPage page = new DepartmentPage();
+            page.ShowDialog();
+            
+            using (PersolEmployeeTrackingContext db = new PersolEmployeeTrackingContext())
+            {
+                List<Department> list = db.Departments.OrderBy(x => x.DepartmentName).ToList();
+                gridDepartment.ItemsSource = list;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            Department dpt = (Department)gridDepartment.SelectedItem;
+            DepartmentPage page = new DepartmentPage();
+            page.department = dpt;
+            page.ShowDialog();
+            using (PersolEmployeeTrackingContext db = new PersolEmployeeTrackingContext())
+            {
+                gridDepartment.ItemsSource = db.Departments.OrderBy(x => x.DepartmentName).ToList();
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Department model = (Department)gridDepartment.SelectedItem;
+            if (model != null && model.Id != 0)
+            {
+                if (MessageBox.Show("Are you sure to delete", "Question", MessageBoxButton.YesNo
+                    , MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    PersolEmployeeTrackingContext db = new PersolEmployeeTrackingContext();
+                    List<Position> positions = db.Positions.Where(x => x.DepartmentId == model.Id).ToList();
+                    foreach (var item in positions)
+                    {
+                        db.Positions.Remove(item);
+                    }
+                    db.SaveChanges();
+
+                    Department department = db.Departments.Find(model.Id);
+                    db.Departments.Remove(department);
+                    db.SaveChanges();
+                    MessageBox.Show("Department was deleted");
+                    gridDepartment.ItemsSource = db.Departments.ToList();
+                }
+            }
         }
     }
 }
